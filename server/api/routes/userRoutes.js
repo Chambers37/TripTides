219df325a7d28395f5888b6a3775a0170fd13a25
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2024-10-26 11:54:02 -0400

    Set up user routes for registration and login

--- server/api/routes/userRoutes.js
+++ server/api/routes/userRoutes.js
@@ -3,3 +3,7 @@ const { registerUser, loginUser } = require('../controllers/userController');
 
 const userRouter = express.Router();
 
+userRouter.post('/register', registerUser);
+userRouter.post('/login', loginUser);
+
+module.exports = userRouter;
