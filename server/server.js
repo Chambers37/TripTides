c7386ae675e7188d4b573e521db413356f279d77
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2024-10-26 09:54:38 -0400

    added server and mongoose connection

--- server/server.js
+++ server/server.js
@@ -5,6 +5,11 @@ const app = express();
 
 connectDB();
 
+// Test route to confirm server and MongoDB connection
+app.get('/', (req, res) => {
+  res.send('Server is running and MongoDB is connected!');
+});
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
